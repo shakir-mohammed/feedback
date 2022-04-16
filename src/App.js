ddd0ef705c805461d66fb50a5c4c99c71e9609c1
Shakir Ahmed <shakir-ahmed@Shakirs-MacBook-Pro.local>
2022-04-16 16:38:37 +0200

    feedback stats

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
 
 
 function App () {
@@ -31,6 +32,7 @@ function App () {
        <>
           <Header />
           <div className='container'>
+             <FeedbackStats feedback={feedback}/>
              <FeedbackList feedback={feedback}
              handleDelete={deleteFeedback}/>
           </div>
